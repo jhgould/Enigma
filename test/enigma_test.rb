0e19101d499195635e2469febcd7512b7e68489b
Jack Gould <johnhilsmangould@gmail.com>
2020-04-17 16:36:27 -0600

    built out methods for shift keys and offsets

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,4 +8,30 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_has_readable_attributes
+    enigma = Enigma.new
+
+    character_set_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal character_set_array, enigma.character_set
+  end
+
+  def test_it_can_generate_rand_key
+
+    enigma = Enigma.new
+
+    assert_instance_of Array, enigma.keys
+    assert_equal 4, enigma.keys.length
+
+  end
+
+  def test_it_can_square
+    enigma = Enigma.new
+    assert_equal [6,4,0,0], enigma.offset
+  end
+
+  def test_it_can_shift
+    enigma = Enigma.new
+    assert_instance_of Hash, enigma.shift
+    assert_equal 4, enigma.shift.length
+  end
 end
