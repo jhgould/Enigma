9842af646861814ca32da384274da3a8960496c8
Jack Gould <johnhilsmangould@gmail.com>
2020-04-18 10:41:09 -0600

    nuked some code. Realized I was going down the wrong path

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,49 +9,38 @@ class Enigma
 
 # creates a 5 digit key and then converts them into strings
 #so that they can be turned into an array
+
   def keys
     key = 5.times.map do |num|
       rand(9).to_s
     end
-    convert_string_of_keys_into_array_of_ints(key)
+    key.join
   end
 
-  def convert_string_of_keys_into_array_of_ints(key)
-    #while not empty? == true
-    string_keys = []
-      loop do
-        if key.length >= 2
-          string_keys << key[0] + key[1]
-          key.shift
-        else
-          break
-        end
-      end
-      string_keys.map{|num| num.to_i}
+  #generates the offset by using todays date or off user input
+  def offset(date = @date)
+    num = date.to_i ** 2
+    off = num.to_s[-4..-1]
   end
+  # num.to_s[-4..-1].split(//).map{|chr| chr.to_i}
 
-  def offset
-    num = @date.to_i ** 2
-    num.to_s[-4..-1].split(//).map{|chr| chr.to_i}
-  end
 
-  def shift
-    shift_keys = [:A, :B, :C, :D]
-    shift_values = []
-    keys.each do |key|
-      offset.each do |num|
-        shift_values << key + num
-      end
-    end
-    shift_keys.zip(shift_values).to_h
-  end
 
-  def convert_string_into_hash(string)
-      binding.pry
-  end
 
-  def encrypt(message, key = shift, date = @date)
-    binding.pry
-  end
+
+
+  # def convert_string_of_keys_into_array_of_ints(key)
+  #   string_keys = []
+  #     loop do
+  #       if key.length >= 2
+  #         string_keys << key[0] + key[1]
+  #         key.shift
+  #       else
+  #         break
+  #       end
+  #     end
+  #     string_keys.map{|num| num.to_i}
+  # end
+  #
 
 end
