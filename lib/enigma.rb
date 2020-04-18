9f61fe8bd6edee2b32384035f3d197b1c3f47f51
Jack Gould <johnhilsmangould@gmail.com>
2020-04-18 12:09:43 -0600

    add shift and shift test method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -20,27 +20,45 @@ class Enigma
   #generates the offset by using todays date or off user input
   def offset(date = @date)
     num = date.to_i ** 2
-    off = num.to_s[-4..-1]
+    num.to_s[-4..-1].split(//).map{|chr| chr.to_i}
   end
-  # num.to_s[-4..-1].split(//).map{|chr| chr.to_i}
+
+  def shift(key, date)
+    shift_keys = [:A, :B, :C, :D]
+    key = convert_string_into_array(key)
+    offset = offset(date)
+    shift_value = key.zip(offset)
+      shift_amount = shift_value.map do |num|
+         num.sum
+      end
+      shift_keys.zip(shift_amount).to_h
+  end
 
 
-
-
-
-
-  # def convert_string_of_keys_into_array_of_ints(key)
-  #   string_keys = []
-  #     loop do
-  #       if key.length >= 2
-  #         string_keys << key[0] + key[1]
-  #         key.shift
-  #       else
-  #         break
-  #       end
-  #     end
-  #     string_keys.map{|num| num.to_i}
-  # end
-  #
+  def convert_string_into_array(string)
+    string_keys = []
+      loop do
+        if string.size >= 2
+          string_keys << string[0] + string[1]
+          string = string[1..-1]
+        else
+          break
+        end
+      end
+      string_keys.map{|num| num.to_i}
+  end
 
 end
+
+
+
+
+
+# shift_value = []
+# key.each do |key_num|
+#   offset.each do |offset_num|
+#     shift_value << key_num + offset_num
+#   end
+# end
+# shift_value
+# binding.pry
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/enigma'
+require 'mocha/minitest'
 
 class EnigmaTest < Minitest::Test
 
@@ -25,9 +26,27 @@ class EnigmaTest < Minitest::Test
 
   def test_it_can_create_offset
     enigma = Enigma.new
-    assert_equal "6400", enigma.offset("180420")
-    assert_equal "6400", enigma.offset(180420)
+    assert_equal [6, 4, 0, 0], enigma.offset("180420")
+    assert_equal [6, 4, 0, 0], enigma.offset(180420)
     assert_equal 4, enigma.offset.length
   end
 
-end 
+  def test_it_can_shift
+    
+    enigma = Enigma.new
+    key = enigma.keys
+    date = enigma.date
+
+    assert_instance_of Hash, enigma.shift('02345', date)
+  end
+
+  def test_it_can_convert_string_to_array
+    enigma = Enigma.new
+    key = enigma.keys
+    date = enigma.date
+
+    assert_instance_of Array, enigma.convert_string_into_array(key)
+    assert_equal 4, enigma.convert_string_into_array(key).length
+  end
+
+end
