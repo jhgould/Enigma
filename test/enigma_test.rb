3d56afff9a00beb06c169b00cfca806406ed2316
Jack Gould <johnhilsmangould@gmail.com>
2020-04-19 10:59:44 -0600

    add decrypt mehtod and test. Works but will need refactor. Does not follow the interatction patern exactly. Will need to update method to take in hash key instead of just string

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,7 +5,6 @@ require 'mocha/minitest'
 class EnigmaTest < Minitest::Test
 
   def test_it_exsists
-
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
@@ -15,25 +14,26 @@ class EnigmaTest < Minitest::Test
     char_set = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
     assert_equal char_set, enigma.character_set
-    assert_equal "180420", enigma.date
+    assert_equal "190420", enigma.date
     assert_equal 5, enigma.key.length
   end
 
   def test_it_can_generate_rand_key
     enigma = Enigma.new
+
     assert_instance_of String, enigma.keys
     assert_equal 5, enigma.keys.length
   end
 
   def test_it_can_create_offset
     enigma = Enigma.new
+
     assert_equal [6, 4, 0, 0], enigma.offset("180420")
     assert_equal [6, 4, 0, 0], enigma.offset(180420)
     assert_equal 4, enigma.offset.length
   end
 
   def test_it_can_shift
-
     enigma = Enigma.new
     key = enigma.keys
     date = enigma.date
@@ -51,12 +51,21 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_encrypt
-
     enigma = Enigma.new
     key = enigma.keys
     date = enigma.date
     hash = {:encryption=>"keder ohulw", :key=>"02715", :date=>"040895"}
+
     assert_equal hash, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt
+
+    enigma = Enigma.new
+    # encrypt = {:encryption=>"keder ohulw", :key=>"02715", :date=>"040895"}
+    decrypt = {:decryption=>"hello world", :key=>"02715", :date=>"040895"}
+
+    assert_equal decrypt, enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
 end
