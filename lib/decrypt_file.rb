3cc801ab44117469ed399761ec48501fe77de427
Jack Gould <johnhilsmangould@gmail.com>
2020-04-20 15:01:28 -0600

    adds a decrypt msg file to work with txt files

--- lib/decrypt_file.rb
+++ lib/decrypt_file.rb
@@ -3,13 +3,9 @@ require './lib/enigma'
 
 enigma = Enigma.new
 handle = File.open(ARGV[0], "r")
-
 incoming_text = handle.read.chomp
-
-# capitalized_text = incoming_text
-encrypted_msg = enigma.encrypt(incoming_text, "02715", "040895")
+encrypted_msg = enigma.decrypt(incoming_text, "02715", "040895")
 writer = File.open(ARGV[1], "w")
-
-writer.write(encrypted_msg[:encryption])
-
+writer.write(encrypted_msg[:decryption])
 writer.close
+#ruby lib/decrypt_file.rb lib/encrypted.txt lib/decrypted_message.txt
