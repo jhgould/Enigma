913099834a94a94fac5969c1eb293221221c2953
Jack Gould <johnhilsmangould@gmail.com>
2020-04-20 15:01:51 -0600

    refactor some of encrypt file

--- lib/encrypt_file.rb
+++ lib/encrypt_file.rb
@@ -3,18 +3,11 @@ require './lib/enigma'
 
 enigma = Enigma.new
 handle = File.open(ARGV[0], "r")
-
 incoming_text = handle.read.chomp
-
-# capitalized_text = incoming_text
 encrypted_msg = enigma.encrypt(incoming_text, "02715", "040895")
 writer = File.open(ARGV[1], "w")
-
 writer.write(encrypted_msg[:encryption])
-
 writer.close
 
 
-#ruby file.rb text.txt capitalized.txt
-# ^ is how you run this file and print to capitalized text
 #ruby lib/encrypt_file.rb lib/encrypt_message.txt lib/encrypted.txt
